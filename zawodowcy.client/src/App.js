d737c577685a9961a11bfc0b1ff61220c316cfcc
oreze <kunas1@o2.pl>
2022-03-23 11:51:01 +0100

    Create testing login button

--- zawodowcy.client/src/App.js
+++ zawodowcy.client/src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { createUserManager } from 'redux-oidc';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Studia nie są obowiązkowe
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          XD
-        </a>
-      </header>
-    </div>
-  );
+    const userManagerConfig = {
+        authority: 'https://localhost:7234',
+        client_id: 'spa',
+        redirect_uri: 'http://localhost:3000/',
+        post_logout_redirect_uri: 'http://localhost:3000/',
+        response_type: 'code',
+        scope: 'openid profile',
+        loadUserInfo: true,
+        monitorSession: false
+    }
+
+    const test = () => {
+        signIn();
+    }
+
+    const userr = () => {
+        userManager.getUser().then((user) => {
+            console.log('THEN');
+            console.log(user)
+        }).catch(error => {
+            console.log('ERROR');
+            console.log(error);
+        })
+    }
+
+
+    function signIn() {
+        return userManager.signinRedirect();
+    }
+    const userManager = createUserManager(userManagerConfig)
+
+
+
+
+
+    return ( <
+        div className = "App" >
+        <
+        button onClick = { test } > login wtf < /button> <
+        button onClick = { userr } > getuser < /button> <
+        /div >
+    );
 }
 
 export default App;
